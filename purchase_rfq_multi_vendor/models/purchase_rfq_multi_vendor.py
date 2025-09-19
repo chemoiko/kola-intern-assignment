7a30dc39583678259107f02342dcd2a2445be7ee
elijah <elijahmurket@gmail.com>
2025-09-19 15:10:06 +0300

    multi vendor search working

--- purchase_rfq_multi_vendor/models/purchase_rfq_multi_vendor.py
+++ purchase_rfq_multi_vendor/models/purchase_rfq_multi_vendor.py
@@ -5,13 +5,6 @@ from odoo import models, fields, api
 class PurchaseOrder(models.Model):
     _inherit = 'purchase.order'
 
-    vendor_ids = fields.Many2many(
-        'res.partner',
-        'purchase_order_vendor_rel',   # relation table name
-        'order_id',                    # column linking to purchase.order
-        'partner_id',                  # column linking to res.partner
-        string='Vendors',
-        domain=[('supplier_rank', '>', 0)]
-    )
+    vendor_ids = fields.Many2many('res.partner','purchase_order_vendor_rel','order_id','partner_id', string='Vendors')
 
 
