b8d02022608651e714d600a08a1290cbed7c86d5
elijah <elijahmurket@gmail.com>
2025-09-19 16:58:13 +0300

    changed relationship to one2many and list type

--- purchase_rfq_multi_vendor/models/purchase_rfq_multi_vendor.py
+++ purchase_rfq_multi_vendor/models/purchase_rfq_multi_vendor.py
@@ -1,10 +1,25 @@
 
-from odoo import models, fields, api
+from odoo import models, fields
 
 
 class PurchaseOrder(models.Model):
     _inherit = 'purchase.order'
 
-    vendor_ids = fields.Many2many('res.partner','purchase_order_vendor_rel','order_id','partner_id', string='Vendors')
+    rfq_vendor_ids = fields.One2many(
+        'purchase.rfq.vendor',
+        'rfq_id',
+        string='Vendors'
+    )
+
+class PurchaseRfqVendor(models.Model):
+    _name = 'purchase.rfq.vendor'
+    _description = 'RFQ Vendor'
+
+    rfq_id = fields.Many2one('purchase.order', string='RFQ', required=True, ondelete='cascade')
+    partner_id = fields.Many2one('res.partner', string='Vendor', required=True, domain=[('supplier_rank', '>=', 0)])
+
+    _sql_constraints = [
+        ('uniq_rfq_partner', 'unique(rfq_id, partner_id)', 'This vendor is already linked to this RFQ.')
+    ]
 
 
