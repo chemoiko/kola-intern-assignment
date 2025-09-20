cb5d7a28cefa4c10169493ce95d5e3000d2969bc
elijah <elijahmurket@gmail.com>
2025-09-20 17:46:52 +0300

    bids checkmark working and autoselecting vendor

--- purchase_rfq_multi_vendor/models/purchase_rfq_multi_vendor.py
+++ purchase_rfq_multi_vendor/models/purchase_rfq_multi_vendor.py
@@ -16,6 +16,16 @@ class PurchaseOrder(models.Model):
         string='Bids'
     )
 
+    def button_confirm(self):
+        res = super().button_confirm()
+        for order in self:
+            winner = order.partner_id
+            if winner and order.rfq_vendor_ids:
+                others = order.rfq_vendor_ids.filtered(lambda v: v.partner_id.id != winner.id)
+                if others:
+                    others.unlink()
+        return res
+
 class PurchaseRfqVendor(models.Model):
     _name = 'purchase.rfq.vendor'
     _description = 'RFQ Vendor'
@@ -41,5 +51,41 @@ class PurchaseRfqBid(models.Model):
     _sql_constraints = [
         ('uniq_rfq_vendor', 'unique(rfq_id, vendor_id)', 'This vendor already has a bid on this RFQ.')
     ]
+
+    def _apply_won_side_effects(self):
+        # Apply side-effects of a bid being marked as won without rewriting state again
+        for bid in self:
+            if not bid.rfq_id:
+                continue
+            other_bids = bid.rfq_id.bid_ids.filtered(lambda b: b.id != bid.id)
+            if other_bids:
+                other_bids.write({'state': 'lost'})
+            link_model = self.env['purchase.rfq.vendor']
+            if not bid.rfq_id.rfq_vendor_ids.filtered(lambda v: v.partner_id.id == bid.vendor_id.id):
+                link_model.create({
+                    'rfq_id': bid.rfq_id.id,
+                    'partner_id': bid.vendor_id.id,
+                })
+            bid.rfq_id.partner_id = bid.vendor_id.id
+
+    def action_set_won(self):
+        for bid in self:
+            if not bid.rfq_id:
+                continue
+            if bid.state != 'won':
+                bid.with_context(skip_won_hook=True).write({'state': 'won'})
+            bid._apply_won_side_effects()
+
+    def write(self, vals):
+        res = super().write(vals)
+        if 'state' in vals and vals['state'] == 'won' and not self.env.context.get('skip_won_hook'):
+            for rec in self.filtered(lambda r: r.state == 'won'):
+                rec._apply_won_side_effects()
+        return res
+
+    def action_set_lost(self):
+        for bid in self:
+            if bid.state != 'lost':
+                bid.write({'state': 'lost'})
 
 
